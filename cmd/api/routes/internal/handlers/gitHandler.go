0d723c96d3ed982db950a713dd0973e1621100d7
shreerajshetty <rajshetty727@gmail.com>
2024-07-15 12:46:03 +0530

    formatted git date

--- cmd/api/routes/internal/handlers/gitHandler.go
+++ cmd/api/routes/internal/handlers/gitHandler.go
@@ -56,6 +56,75 @@
 
 // with pagination
 
+// package handlers
+
+// import (
+// 	"context"
+// 	"dashboard/cmd/api/routes/internal/database"
+// 	"dashboard/cmd/api/routes/internal/helpers"
+// 	"dashboard/cmd/api/routes/internal/models"
+// 	"net/http"
+// 	"strconv"
+
+// 	"go.mongodb.org/mongo-driver/bson"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// func FetchGitMetrics(userName, repoName string, limit, skip int64) ([]models.GitMetric, error) {
+// 	filter := bson.M{}
+// 	if userName != "None" {
+// 		filter["commited_by"] = userName
+// 	}
+// 	if repoName != "None" {
+// 		filter["reponame"] = repoName
+// 	}
+// 	opts := options.Find().SetSort(bson.D{{Key: "commit_date", Value: -1}}).SetLimit(limit).SetSkip(skip)
+
+// 	cursor, err := database.GitMetricsCollection.Find(context.Background(), filter, opts)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer cursor.Close(context.Background())
+
+// 	return helpers.DecodeCursor[models.GitMetric](context.Background(), cursor)
+// }
+
+// func GitMetricsHandler(w http.ResponseWriter, r *http.Request) {
+// 	userName := r.URL.Query().Get("user_name")
+// 	repoName := r.URL.Query().Get("repo_name")
+// 	pageStr := r.URL.Query().Get("page")
+// 	page, err := strconv.ParseInt(pageStr, 10, 64)
+// 	if err != nil || page < 1 {
+// 		page = 1
+// 	}
+
+// 	limit := int64(10)
+// 	skip := (page - 1) * limit
+
+// 	metrics, err := FetchGitMetrics(userName, repoName, limit, skip)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// Add pagination details
+// 	data := models.GitMetricsViewData{
+// 		Metrics:           metrics,
+// 		Repos:             nil,
+// 		Authors:           nil,
+// 		RepoNameParameter: repoName,
+// 		UserNameParameter: userName,
+// 		CurrentPage:       page,
+// 		PreviousPage:      page - 1,
+// 		NextPage:          page + 1,
+// 	}
+
+// 	if err := helpers.RenderTemplate(w, data, "git_dashboard.html"); err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// }
+
 package handlers
 
 import (
@@ -105,6 +174,11 @@ func GitMetricsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+
+	// Format commit dates
+	for i := range metrics {
+		metrics[i].FormattedCommitDate = metrics[i].CommitDate.Format("Jan 2, 2006 at 3:04pm")
 	}
 
 	// Add pagination details
--- cmd/api/routes/internal/models/git_metrics.go
+++ cmd/api/routes/internal/models/git_metrics.go
@@ -3,18 +3,19 @@ package models
 import "time"
 
 type GitMetric struct {
-	ID            string    `bson:"_id,omitempty"`
-	CommitMessage string    `bson:"commit_message"`
-	FilesDeleted  int       `bson:"files_deleted"`
-	FilesUpdated  int       `bson:"files_updated"`
-	LinesDeleted  int       `bson:"lines_deleted"`
-	CommitID      string    `bson:"commit_id"`
-	CommittedBy   string    `bson:"commited_by"`
-	FilesAdded    int       `bson:"files_added"`
-	LinesAdded    int       `bson:"lines_added"`
-	LinesUpdated  int       `bson:"lines_updated"`
-	RepoName      string    `bson:"reponame"`
-	CommitDate    time.Time `bson:"commit_date"`
+	ID                  string    `bson:"_id,omitempty"`
+	CommitMessage       string    `bson:"commit_message"`
+	FilesDeleted        int       `bson:"files_deleted"`
+	FilesUpdated        int       `bson:"files_updated"`
+	LinesDeleted        int       `bson:"lines_deleted"`
+	CommitID            string    `bson:"commit_id"`
+	CommittedBy         string    `bson:"commited_by"`
+	FilesAdded          int       `bson:"files_added"`
+	LinesAdded          int       `bson:"lines_added"`
+	LinesUpdated        int       `bson:"lines_updated"`
+	RepoName            string    `bson:"reponame"`
+	CommitDate          time.Time `bson:"commit_date"`
+	FormattedCommitDate string    // New field for formatted date
 }
 
 // GitMetricsViewData holds the data for rendering the Git metrics view
